9c7ed2131909254667a3261f3468095e4bcca4a0
RagibAhashan <ahashanragib@gmail.com>
2022-08-16 22:35:36 -0400

    solution

--- bridgetwo/lib.cpp
+++ bridgetwo/lib.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "osrm/match_parameters.hpp"
 #include "osrm/nearest_parameters.hpp"
 #include "osrm/route_parameters.hpp"
 #include "osrm/table_parameters.hpp"
@@ -13,11 +12,7 @@
 #include "osrm/engine_config.hpp"
 #include "osrm/json_container.hpp"
 
-#include "engine/api/match_parameters.hpp"
-#include "engine/api/nearest_parameters.hpp"
-#include "engine/api/route_parameters.hpp"
-#include "engine/api/table_parameters.hpp"
-#include "engine/api/trip_parameters.hpp"
+#include "osrm/engine/api/match_parameters.hpp"
 
 #include "engine/engine_config.hpp"
 #include "engine/status.hpp"
@@ -37,6 +32,7 @@
 #include <string>
 
 using namespace osrm;
+using namespace std;
 
 extern "C" {
     void test() { std::cout << "test\n"; }
@@ -106,50 +102,54 @@ extern "C" {
 
 
 
-        engine::api::ResultT result;
+        engine::api::ResultT result = json::Object();
         try
-        {   
+        {  
+            // Status Match(const MatchParameters &parameters, engine::api::ResultT &result) const; 
             const auto status = osrm.Match(match_param, result);
+            auto &json_result = result.get<json::Object>();
+
+
+
+            if (status == Status::Ok)
+            {
+                auto &routes = json_result.values["tracepoints"].get<json::Array>();
+
+                for (int i = 0; i < json_result.values["tracepoints"].get<json::Array>().values.size(); i++)
+                {
+                    auto &route = routes.values.at(i).get<json::Object>();
+                    auto &location = route.values["location"].get<json::Array>();
+
+                    cout << location.values.at(0).get<json::Number>().value << " , "<<  location.values.at(1).get<json::Number>().value << endl;
+
+                    
+
+                    //Filling the vector Up - lon and lat
+                    // locVector.emplace_back(location.values.at(0).get<json::Number>().value, location.values.at(1).get<json::Number>().value);
+                }
+
+                // Let's just use the first route
+                // auto &route = routes.values.at(0).get<json::Object>();
+                // const auto distance = route.values["distance"].get<json::Number>().value;
+                // const auto duration = route.values["duration"].get<json::Number>().value;
+
+                // // Warn users if extract does not contain the default coordinates from above
+                // if (distance == 0 || duration == 0)
+                // {
+                //     std::cout << "Note: distance or duration is zero. ";
+                //     std::cout << "You are probably doing a query outside of the OSM extract.\n\n";
+                // }
+
+                // std::cout << "Distance: " << distance << " meter\n";
+                // std::cout << "Duration: " << duration << " seconds\n";
+                return 1;
+            }
+            
         }
         catch(const std::exception& e)
         {
             std::cerr << e.what() << '\n';
         }
-        
-        
-
-
-        // auto &json_result = result.get<json::Object>();
-        // if (status == Status::Ok)
-        // {
-        //     auto &routes = json_result.values["routes"].get<json::Array>();
-
-        //     // Let's just use the first route
-        //     auto &route = routes.values.at(0).get<json::Object>();
-        //     const auto distance = route.values["distance"].get<json::Number>().value;
-        //     const auto duration = route.values["duration"].get<json::Number>().value;
-
-        //     // Warn users if extract does not contain the default coordinates from above
-        //     if (distance == 0 || duration == 0)
-        //     {
-        //         std::cout << "Note: distance or duration is zero. ";
-        //         std::cout << "You are probably doing a query outside of the OSM extract.\n\n";
-        //     }
-
-        //     std::cout << "Distance: " << distance << " meter\n";
-        //     std::cout << "Duration: " << duration << " seconds\n";
-        //     return EXIT_SUCCESS;
-        // }
-        // else if (status == Status::Error)
-        // {
-        //     const auto code = json_result.values["code"].get<json::String>().value;
-        //     const auto message = json_result.values["message"].get<json::String>().value;
-
-        //     std::cout << "Code: " << code << "\n";
-        //     std::cout << "Message: " << code << "\n";
-        //     return EXIT_FAILURE;
-        // }
-
         return 1;
     }
 }
